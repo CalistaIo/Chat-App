4943e2c8ba183444c6cf97ecb3af46a2a1b1f47a
CalistaIo <e0426281@u.nus.edu>
2021-02-11 10:42:10 +0800

    Notify users when new user is connected/disconnected

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,20 @@
-io();
+const socket = io();
+// const button = document.getElementById('increment');
+
+// socket.on('countUpdated', (count) => {
+//     console.log('The count has been updated!', count);
+// });
+
+// button.onclick = () => {
+//     socket.emit('increment');
+// }
+
+socket.on('message', (mesg) => {
+    console.log(mesg);
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const mesg = e.target.elements.message.value;
+    socket.emit('sendMessage', mesg);
+});
--- src/index.js
+++ src/index.js
@@ -10,9 +10,29 @@ const io = socketio(server);
 const publicDirectoryPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicDirectoryPath));
 
+// let count = 0;
+
 // Listen to event to occur
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New WebSocket connection');
+    socket.emit('message', 'Welcome!'); // emit to current socket
+    socket.broadcast.emit('message', 'A new user has joined!'); // emit to all sockets other than current socket
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!');
+    })
+    // socket.emit('countUpdated', count);
+
+    // socket.on('increment', () => {
+    //     count++;
+    //     // socket.emit('countUpdated', count);
+    //     // use io.emit() instead of socket.emit() to emit event to all clients in real time
+    //     io.emit('countUpdated', count);
+    // });
+
+    socket.on('sendMessage', (mesg) => {
+        io.emit('message', mesg);
+    })
 });
 
 server.listen(port, () => {
